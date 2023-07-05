bac0f30744937267c7e488516d4f7f69abf1b2d5
UIMSolutions <ui.manufaktur@gmail.com>
2023-07-05 14:09:16 +0200

    Rename namespace uim.sql to langs.sql

--- source/uim/sql/statements/package.d
+++
@@ -1,5 +0,0 @@
-﻿module uim.sql.statements;
-
-public import uim.sql.statements.statement;
-public import uim.sql.statements.update;
-public import uim.sql.statements.query;
--- source/uim/sql/statements/query.d
+++
@@ -1,9 +0,0 @@
-﻿module uim.sql.statements.query;
-
-import uim.sql;
-
-class DSQLQueryStatement : DSQLStatement {
-	this() {}
-}
-auto SQLQueryStatement() { return new DSQLQueryStatement; }
-
