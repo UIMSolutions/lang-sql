a56332b942663a03a7b309ab33e7e60387130a42
UIMSolutions <ui.manufaktur@gmail.com>
2023-07-05 14:09:04 +0200

    Rename namespace uim.sql to langs.sql

---
+++ source/langs/sql/table/altertable.d
@@ -0,0 +1,29 @@
+﻿module uim.sql.tables.altertable;
+
+import uim.sql;
+
+string sqlAlterTable(string tableName) { return "ALTER "~tableName; }
+string sqlAddColumn(string columnName, string definition) { return "ADD "~columnName~" "~definition; }
+string sqlAddColumn(string sql, string columnName, string definition) { return sql~" "~sqlAddColumn(columnName, definition); }
+
+class DSQLAlterTable : DSQLUpdateStatement {
+	this() { super(); }
+	this(string aTableName) { this(); _tableName = aTableName; }
+	mixin(OProperty!("string", "tableName")); 
+
+	override string toSQL() {
+		return "ALTER %s".format(_tableName); // ADD %s %s".format(_tableName, _columnName, _columnDefinition);
+	}
+}
+auto SQLAlterTable() { return new DSQLAlterTable(); }
+auto SQLAlterTable(string aTableName) { return new DSQLAlterTable(aTableName); }
+
+unittest {
+	writeln("Testing ", __MODULE__);
+
+	assert(sqlAlterTable("aTable") == "ALTER aTable");
+	assert(sqlAddColumn("xxx", "yyy") == "ADD xxx yyy");
+	assert(sqlAlterTable("aTable").sqlAddColumn("xxx", "yyy") == "ALTER aTable ADD xxx yyy");
+
+	assert(SQLAlterTable("aTable") == "ALTER aTable");
+}
--- source/uim/sql/README.md
+++
@@ -1,3 +0,0 @@
-# Libary uim-sql
-
-SQL is a standard language for storing, manipulating and retrieving data in databases.
