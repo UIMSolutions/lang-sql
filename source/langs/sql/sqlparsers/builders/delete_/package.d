8c5e14a5583989a87d964c218ce6abd450d82e6a
UIMSolutions <ui.manufaktur@gmail.com>
2023-12-29 15:40:56 +0100

    Updates

--- source/langs/sql/sqlparsers/builders/delete_/package.d
+++
@@ -1,5 +0,0 @@
-module langs.sql.sqlparsers.builders.delete_;
-
-public {
-  import langs.sql.sqlparsers.builders.delete_.statement;
-}
--- source/langs/sql/sqlparsers/builders/groupby/package.d
+++
@@ -1,2 +0,0 @@
-module langs.sql.sqlparsers.builders.groupby;
-
